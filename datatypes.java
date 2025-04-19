2e5dd94dd3a535b9e9ab5e9dc69d27791983684c
pallakk21 <palakgupta2135@gmail.com>
2025-04-19 09:39:07 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,18 @@
+public class datatypes{
+    public static void main(String[] args){
+       byte a=12;
+        System.out.println(a);
+       
+         int b=34;
+        System.out.println(b);
+        short sh=13;
+        System.out.println(sh);
+        boolean bl=true;
+        System.out.println(bl);
+        double c=0.5;
+        System.out.println(c);
+        float d=44.4F;
+        long l=45678932;
+       System.out.println(l);
+    }
+}    
