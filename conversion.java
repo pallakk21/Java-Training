750fd815a97e75f4713acd7419df194022123c7f
pallakk21 <palakgupta2135@gmail.com>
2025-04-23 10:46:22 +0530

    conversion.java

--- conversion.java
+++ conversion.java
@@ -1,4 +1,4 @@
-/*public class conversion {
+public class conversion {
     public static void  main(String[] args){
         short s= 7;
         byte b = (byte) s;
@@ -19,36 +19,4 @@
 
     }
     
-}*/
-import java.util.Scanner;
-
-public class switch {
-   public static void main(String[] var0) {
-      Scanner var1 = new Scanner(System.in);
-      System.out.println("Enter a first number");
-      int var2 = var1.nextInt();
-      System.out.println("Enter a second number");
-      int var3 = var1.nextInt();
-      System.out.println("Enter your operators value");
-      char var4 = var1.next().charAt(0);
-      switch(var4) {
-      case '*':
-         System.out.println(var2 * var3);
-         break;
-      case '+':
-         System.out.println(var2 + var3);
-      case ',':
-      case '.':
-      default:
-         break;
-      case '-':
-         System.out.println(var2 - var3);
-         break;
-      case '/':
-         System.out.println(var2 / var3);
-      }
-
-   }
 }
-
-
