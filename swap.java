6fcaf83bdc16f0b401c666672492d1fa980d4e0c
pallakk21 <palakgupta2135@gmail.com>
2025-04-24 18:22:19 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,28 @@
+public class swap {
+    public static void main(String[] args) {
+        //using third variable
+      /*  int a , b, Swap;
+        a=23;
+        b=20;
+        Swap =a;
+        a=b;
+        b = Swap;
+        System.out.println(a);
+        System.out.println(b);*/
+         
+
+
+        // without third variable
+        int a=4 , b=8 ;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        
+         System.out.println(a);
+         System.out.println(b);
+
+
+
+    }
+}
+
