311de452503cacf9cd4519f70d91a8056a5c5847
pallakk21 <palakgupta2135@gmail.com>
2025-04-25 13:38:48 +0530

    Add files via upload

---
+++ statement.java
@@ -0,0 +1,28 @@
+/*public class statement {
+    public static void main(String[] args){
+        String name = "Palak";
+        int age = 20;
+        System.out.println("My name is  "+ name +  " and I am " + age + " years old");
+    }
+}
+import java.util.Scanner;
+public class statement {
+    public static void main(String[] args) {
+       
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your name");
+        String name =sc.nextLine();
+        System.out.println(name);
+    }
+}
+*/
+import java.util.Scanner;
+public class statement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your age");
+        String age = sc.nextLine();
+        System.out.println(age);
+
+    }
+}
